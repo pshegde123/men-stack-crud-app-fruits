9404de3d7e7ac74d8d60a0ec8589ed579fb01e93
pshegde123 <pradnyahegde@gmail.com>
2025-09-03 11:15:05 -0400

    added mongodb connection check

--- server.js
+++ server.js
@@ -3,8 +3,16 @@
 const dotenv = require("dotenv"); // require package
 dotenv.config(); // Loads the environment variables from .env file
 const express = require('express');
+const mongoose = require("mongoose"); 
 const app = express();
 //const PORT = 3000;
+
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
