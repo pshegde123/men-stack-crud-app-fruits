26c1d0b993e3d901c98b546582bff9a27be1cad7
pshegde123 <pradnyahegde@gmail.com>
2025-09-03 11:09:36 -0400

    setup env variables

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Here is where we import modules
 // We begin by loading Express
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
 const express = require('express');
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
